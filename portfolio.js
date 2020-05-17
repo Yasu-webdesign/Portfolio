8108f61c2e52ca712735ec17488c0e38898b0430
Yasu-webdesign <63635804+Yasu-webdesign@users.noreply.github.com>
2020-05-17 22:33:52 +0900

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,54 @@
+$(function(){
+
+  $('.fa-bars').click(function(){
+    var $burger = $('.hidden');
+    if($burger.hasClass('close')){
+      $burger.removeClass('close');
+      $burger.slideDown();
+    } else {
+      $burger.addClass('close');
+      $burger.hide();
+    }
+  });
+
+
+  $('#head-title').click(function(){
+    $('html').animate({scrollTop : 0},500);
+  });
+
+  $('#smarthead').click(function(){
+    $('html').animate({scrollTop : 0},500);
+  });
+
+  $('header a').click(function(){
+      var id = $(this).attr('href');
+      var position = $(id).offset().top;
+      $('html,body').animate({
+        'scrollTop': position - 60
+      }, 500);
+    });
+
+    $('a').click(function(){
+        var id = $(this).attr('href');
+        var position = $(id).offset().top;
+        $('html,body').animate({
+          'scrollTop': position - 60
+        }, 500);
+      });
+
+  $('.button').click(function(){
+      var id = $(this).attr('href');
+      var position = $(id).offset().top;
+      $('html,body').animate({
+        'scrollTop': position - 60
+      }, 500);
+    });
+
+  $('.footer a').click(function(){
+      var id = $(this).attr('href');
+      var position = $(id).offset().top;
+      $('html,body').animate({
+        'scrollTop': position - 100
+      }, 500);
+    });
+});
