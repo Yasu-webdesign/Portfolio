8d06c0e80a314c20e7f186107108c0ffedc345e5
Yasu-webdesign <63635804+Yasu-webdesign@users.noreply.github.com>
2020-05-19 09:10:18 +0900

    Add files via upload

--- portfolio.js
+++ portfolio.js
@@ -53,13 +53,13 @@ $(function(){
     });
 
   $(window).scroll(function (){
-	$('.fadein').each(function(){
-	var elemPos = $(this).offset().top;
-	var scroll = $(window).scrollTop();
-	var windowHeight = $(window).height();
-	if (scroll > elemPos - windowHeight - 70){
-		$(this).addClass('scrollin');
-	  }
-	 });
+		$('.fadein').each(function(){
+			var elemPos = $(this).offset().top;
+			var scroll = $(window).scrollTop();
+			var windowHeight = $(window).height();
+			if (scroll > elemPos - windowHeight - 70){
+				$(this).addClass('scrollin');
+			}
+		});
 	});
 });
